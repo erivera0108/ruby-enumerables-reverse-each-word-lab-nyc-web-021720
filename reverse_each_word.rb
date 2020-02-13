41d2baa66180ad93297867a25c0974cc59def8cc
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-13 23:02:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,4 @@
-# each does not return mulated
+# each does not return mulated data. Any changes must be returned by the user!!
 =begin
 def reverse_each_word (sentence)
  new = []
@@ -8,9 +8,10 @@ def reverse_each_word (sentence)
 end 
 =end 
 
+#Collect returns mulated data 
 def reverse_each_word(sentence)
   string_to_array = sentence.split
   #print string_to_array
-  string_to_array.collect {|word| word.reverse }.join(" ")
+  string_to_array.collect {|word| word.reverse}.join(" ")
 end 
 
