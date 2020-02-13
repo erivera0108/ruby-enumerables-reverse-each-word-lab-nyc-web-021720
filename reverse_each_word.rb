4f6bc93134d7217cc4de0f09447a24c9ac6099b6
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-13 22:00:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,6 @@
 def reverse_each_word(sentence)
   string_to_array = sentence.split
   #print string_to_array
-  string_to_array.each {|word| word.reverse}.join(" ")
-  string_to_array
+  new = string_to_array.each {|word| word.reverse}.join(" ")
+  new 
 end 
